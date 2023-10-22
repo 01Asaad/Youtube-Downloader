1104b9d59c18c470b64239b36482bdfe24ebaf23
Asaad Ali <asaad757.asad@gmail.com>
2023-10-22 11:02:28 +0300

    more responsive and descriptive  gui

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import ytd
+import threading
 
 def toggle_resolution_dropdown():
     if audio_only_var.get():
@@ -9,16 +10,34 @@ def toggle_resolution_dropdown():
         resolution_dropdown.config(state=tk.NORMAL)
 
 def download_video():
+    # Disable the "Download" button to prevent multiple clicks
+    download_button.config(state=tk.DISABLED)
+    
+    # Clear the status label
+    status_label.config(text="")
+
     video_url = link_entry.get()
     resolution = resolution_var.get()
     audio_only = audio_only_var.get()
-    options={}
+    options = {}
     options["video_url"] = video_url
-    options["only_audio"]=audio_only
+    options["only_audio"] = audio_only
     options["resolution"] = resolution
-    ytd.download_video(options)
     
-    
+    # Create a separate thread for the download process
+    download_thread = threading.Thread(target=download_threaded, args=(options,))
+    download_thread.start()
+
+def download_threaded(options):
+    status_label.config(text="Downloading..")
+    try:
+        ytd.download_video(options)
+        status_label.config(text="Downloaded successfully")
+    except Exception as e:
+        status_label.config(text=f"Failed to download: {str(e)}")
+    finally:
+        # Enable the "Download" button after download is complete
+        download_button.config(state=tk.NORMAL)
 
 # Create a Tkinter window
 window = tk.Tk()
--- ytd.py
+++ ytd.py
@@ -1,8 +1,11 @@
 from pytube import YouTube
+from pytube import exceptions
 from moviepy.editor import AudioFileClip
   
 def download_video(options) :
-    yt = YouTube(options["video_url"])
+    try : yt = YouTube(options["video_url"])
+    except exceptions.RegexMatchError : raise Exception("Failed to get the video, make sure the link is valid")
+    except : raise Exception("Failed to get the video, make sure your internet is working and the link is valid")
 
     if options["only_audio"] :
         stream = yt.streams.filter(only_audio=True).first()
@@ -31,7 +34,7 @@ if __name__ == "__main__" :
             options=json.loads(f.read())
     except :
         options={}
-        options["video_url"] = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        options["video_url"] = "    "
         options["only_audio"]=True
         options["resolution"] = '720p'
     download_video(options)
